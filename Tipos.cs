865ffaa52fd2affa590db8e335af0c4e6af4b181
Martin <mujica0106@gmail.com>
2024-10-06 19:57:33 -0300

    Se agrega clase Tipos

---
+++ Tipos.cs
@@ -0,0 +1,51 @@
+namespace ProyectoPokemon;
+
+public class Tipos
+{
+    private string nombre;
+    private List<Tipos> debilidades;
+    private List<Tipos> fortalezas;
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set { nombre = value; }
+    }
+
+    public List<Tipos> Debilidades
+    {
+        get { return debilidades; }
+        set { debilidades = value; }
+    }
+
+    public List<Tipos> Fortalezas
+    {
+        get { return fortalezas; }
+        set { fortalezas = value; }
+    }
+
+    public Tipos(string nombre)
+    {
+        this.Nombre = nombre;
+    }
+
+    public void agregarDebilidades(Tipos tipo)
+    {
+        this.Debilidades.Add(tipo);
+    }
+    
+    public void agregarFortalezas(Tipos tipo)
+    {
+        this.Fortalezas.Add(tipo);
+    }
+    
+    public void eliminarDebilidades(Tipos tipo)
+    {
+        this.Debilidades.Remove(tipo);
+    }
+    
+    public void eliminarFortalezas(Tipos tipo)
+    {
+        this.Fortalezas.Remove(tipo);
+    }
+}
